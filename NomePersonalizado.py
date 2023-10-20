bc6660df0104d872d9da56ef733f3b2cc1d2593a
Thiago <thiago.info.senai@gmail.com>
2023-10-20 18:24:11 -0300

    Adicionando novo arquivo python Nome Personalizado

---
+++ NomePersonalizado.py
@@ -0,0 +1,5 @@
+#Escreva um programa que peça ao usuário para digitar seu nome e, em seguida, imprima uma saudação personalizada.
+
+nome = input('Digite o seu nome: ')
+
+print ('Ola', nome, 'como voce está? Sou sua assistente virtual e estou aqui para ajudar. ')
