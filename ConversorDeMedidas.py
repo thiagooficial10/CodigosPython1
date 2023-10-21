e40c8ddff2401325daf8868789eec5c807a2c7f5
Thiago <thiago.info.senai@gmail.com>
2023-10-21 07:54:11 -0300

    Adicionando novo arquivo conversor de medidas

---
+++ ConversorDeMedidas.py
@@ -0,0 +1,6 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+metros = float(input('Digite os metros: '))
+
+print('A medida de ', metros, 'metros corresponde a: ', metros * 100, 'centimetros')
+print('A medida de ', metros, 'metros corresponde a: ', metros * 1000, 'milimetros')
