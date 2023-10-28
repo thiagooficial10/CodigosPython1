f67a6a7bf6ce31c8f168929db0bab7e6ca97b037
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2023-10-28 13:06:44 -0300

    Update ConversorMoedas.py

--- ConversorMoedas.py
+++ ConversorMoedas.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
-# :.2F  ----------> significa formatar em 2 casas decimais flutuantes
+# :.2F  ----------> significa formatar em 2 casas decimais flutuantes.
 
 real = float(input('digite quanto dinheiro voce tem na carteira: R$'))
 dolar = real / 3.27
