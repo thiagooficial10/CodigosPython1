a3344a85155d8b84a898adc662c29897a3b90986
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2025-02-02 14:47:18 -0300

    Create largura x altura.py

---
+++ largura x altura.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
+larg = float (input('largura da parede'))
+alt = float (input('altura da parede'))
+area = larg * alt
+print ('sua parede tem a dimensao de {}x{} e sua area é de {}m².'.format(larg, alt, area))
+
+tinta = area /2
+
+print ('para pintar essa parede, voce precisara de {}l de tinta.'.format(tinta))
