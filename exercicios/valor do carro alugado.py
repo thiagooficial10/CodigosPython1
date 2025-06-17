edaa07fbe07c1aaf4dafcd87eb0a93973fa650c9
Thiago Ferreira <75267419+thiagooficial10@users.noreply.github.com>
2025-06-17 16:06:10 -0300

    Update valor do carro alugado.py

--- exercicios/valor do carro alugado.py
+++ exercicios/valor do carro alugado.py
@@ -1,10 +1,10 @@
---Pede ao usuário para digitar quantos dias o carro foi alugado. O valor é convertido para inteiro e armazenado na variável dias.
---Pede ao usuário para informar quantos quilômetros foram rodados. O valor é convertido para número decimal (float) e armazenado na variável km.
---Calcula o valor total a pagar:
-R$ 60,00 por cada dia alugado
-R$ 0,15 por cada quilômetro rodado
+#--Pede ao usuário para digitar quantos dias o carro foi alugado. O valor é convertido para inteiro e armazenado na variável dias.
+#--Pede ao usuário para informar quantos quilômetros foram rodados. O valor é convertido para número decimal (float) e armazenado na variável km.
+#--Calcula o valor total a pagar:
+#R$ 60,00 por cada dia alugado
+#R$ 0,15 por cada quilômetro rodado
 
---Mostra o valor total com duas casas decimais.*/
+#--Mostra o valor total com duas casas decimais.*/
 
 dias = int (input('Quantos dias o carro foi alugado?'))
 km = float (input('Quantos km foram rodados? '))
